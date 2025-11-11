533d8ea3aa7c5c7098a13f36db4e0cf4135eccc3
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-11-11 22:35:00 +0100

    Add some IVT dumping to io.c

--- io.c
+++ io.c
@@ -25,6 +25,14 @@ static void printhex(uint8_t x)
     putchar(hexch(x & 0xF));
 }
 
+static void print_u32(uint32_t x)
+{
+    printhex(x >> 24);
+    printhex(x >> 16);
+    printhex(x >> 8);
+    printhex(x);
+}
+
 __attribute__ ((noinline))
 static void dump(uint8_t *buf, uint16_t size)
 {
@@ -35,9 +43,26 @@ static void dump(uint8_t *buf, uint16_t size)
     puts("\r\n");
 }
 
+__attribute__ ((noinline))
+static void dump_far(uint8_t __far* buf, uint16_t size)
+{
+    for (uint16_t i = 0; i < size; i++)
+    {
+        printhex(buf[i]);
+    }
+    puts("\r\n");
+}
+
+typedef uint8_t __far* farptr_t;
+
 __attribute__((noreturn, section(".text.start"))) void _start()
 {
-    puts("Hello from IO.SYS!\r\n");
-    dump((uint8_t*)0x0700, 200);
+    puts("IVT:\r\n");
+    dump((uint8_t*)0x0000, 0x400);
+    puts("BDA:\r\n");
+    dump((uint8_t*)0x400, 0x100);
+    farptr_t* ivt = (farptr_t*)0x0000;
+    puts("EGA characters:\r\n");
+    dump_far(ivt[0x1F], 0x16); // dump INT 0x1E vector table
     while(1);
 }
